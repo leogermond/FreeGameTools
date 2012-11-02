6bcdc53e1aa24392eef30a81083b654cce550f80
Léo Germond <leo.germond@gmail.com>
2012-11-02 20:17:42 +0100

    Animator: Added copyright notice

--- animator.py
+++ animator.py
@@ -2,6 +2,37 @@
 from pygame.locals import *
 from pygame import Color
 
+"""
+    Animator 
+    
+    Animator fait partie de la suite logicielle FreeGameTools, il sert
+    à tester de manière rapide des animations de sprites à différentes
+    vitesses et différents niveaux de zoom.
+    
+    Le programme charge les nouveaux sprites automatiquement et dans
+    l'ordre alphabétique, ce qui permet de tester les animations tout
+    en éditant les sprites et sans avoir à relancer le programme.
+    
+    Animator is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+    
+    Copyright 2012, Léo Germond
+    
+    
+"""
+
+
+
 # La version du logicel
 __version__ = "0.1"
 
